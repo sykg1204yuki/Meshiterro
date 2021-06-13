bc2b8a846fa4025903d86b62e35bdfedc82253ad
sykg1204yuki <y.yuki040507@gmail.com>
2021-06-13 10:16:06 +0000

    Meshiterroアプリ作成【4章　いいね機能作成まで】

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '3be6fcf3ff70e3c785c5d12a50f8e56f7712587beca50ce851a8fa37d7c95191565cf9b3709070d312a4a2848da81fd600456050971ce98772d4da33fe9df140'
